08487e3ba16c681d7795dba83052e6a7f6c14979
thedanieljk <thedanieljk@gmail.com>
2018-09-27 13:29:16 -0700

    edit comments

--- lab1/pipeit.c
+++ lab1/pipeit.c
@@ -4,8 +4,6 @@
  * Patrick Farrell (pifarrel)
  */
 
-/* STILL NOT GETTING OUTFILE:PERMISSION DENIED EXIT STATUS TO BE 1 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -19,68 +17,75 @@ static void checkFork(pid_t p) {
    }
 }
 
-//stdout dup2(fd[1] write end
-
 int main(int argc, char * argv[]) {
    pid_t pid_child1;
    pid_t pid_child2;
 
    int fd1[2];
 
-
+   /* Create the pipe in the parent */
    if (pipe(fd1) == -1) {
       fprintf(stderr, "Pipe failed");
       exit(1);
    }
 
+   /* Fork and check status */
    pid_child1 = fork();
    checkFork(pid_child1); 
 
-   if (pid_child1 == 0) { /* In child 1 (ls?) */
+   if (pid_child1 == 0) { /* In child 1 (ls) */
       if (dup2(fd1[1],STDOUT_FILENO) < 0) {
          perror("error");
          exit(3);
       }
+      /* close read end of pipe */
       close(fd1[0]);
       execlp("ls","ls",NULL); /* exec ls. if runs past, error */
       perror("error");
       exit(1);
    }
    else { /* in parent */
-      pid_t child1Exit;
-      child1Exit = waitpid(pid_child1);
-      if (child1Exit == -1) {
+      int status;
+      /*wait for first child, exit parent if error*/
+      waitpid(pid_child1,&status,0);
+      if (WEXITSTATUS(status)!= 0) {
          perror("child1 exit error");
          exit(1);
       }
-
+      
+      /* fork and check */
       pid_child2 = fork();
-      checkFork(pid_child2); /* Call helper function to check if forked correctly */
-      if (pid_child2 == 0) { /* In child 2 (sort?) */
+      checkFork(pid_child2); 
+      if (pid_child2 == 0) { /* In child 2 (sort) */
          close(fd1[1]);
          if (dup2(fd1[0],STDIN_FILENO) < 0) { 
             perror("error");
             exit(1);
          }
+         /* open and check outfile */
          int outfile = open("outfile",O_RDWR | O_CREAT,0777);
          if (outfile < 0) {
             perror("outfile");
             exit(EXIT_FAILURE);
          }
-         if (dup2(outfile,STDOUT_FILENO) < 0) {/* Write from STDOUT */
+         /* redirect stdout to outfile */
+         if (dup2(outfile,STDOUT_FILENO) < 0) {
             perror("error");
             exit(1);
          }
          close(outfile);
-         execlp("sort","sort","-r", NULL); /* exec sort. If runs past, error */
-         perror(" exec error");
+         /* exec sort. If runs past, error */
+         execlp("sort","sort","-r", NULL); 
+         perror("sort exec error");
          exit(1);
       }
       else { /* Back in parent */
          int status;
+         /* close both ends of pipe in parent */
          close(fd1[0]);
          close(fd1[1]);
-         waitpid(pid_child2,&status,0); /*wait for second child*/
+         /* wait for child 2, exit if failure */
+         waitpid(pid_child2,&status,0);
          if (WEXITSTATUS(status) != 0) {
             exit(1);
          }
