7cf74ba33fe14916106f731e223ef44424fab54b
Pat-Farrell-17 <38262281+Pat-Farrell-17@users.noreply.github.com>
2018-10-11 18:56:44 -0700

    Added lwp.c

--- Assignment2/lwp.c
+++ Assignment2/lwp.c
@@ -1,8 +1,47 @@
-/* CSC 453-03 Assignment 2
- * Daniel Kirkpatrick
- * Patrick Farrell
- * Due 10/12/18
+/* Authors:    Patrick Farrell
+ *             Daniel Kirkpatrick
+ * Course:     CPE 453
+ * Asignment:  2
+ * Instructor: Dr. NIco
  */
 
+#include "lwp.h"
+
+te(lwpfun,void *,size_t);
+void  lwp_exit(void);
+tid_t lwp_gettid(void);
+void  lwp_yield(void);
+void  lwp_start(void);
+void  lwp_stop(void);
+void  lwp_set_scheduler(scheduler fun);
+
+scheduler lwp_get_scheduler(void) {
+     
+
+}
+
+void initRR(void) {
+
+}
+
+void shutdownRR(void) {
+
+}
+
+void admitRR(thread new) {
+
+}
+
+void removeRR(thread victim) {
+
+}
+
+thread nextRR(void) {
+
+}
+
+
+
+thread tid2thread(tid_t tid);
 
 
